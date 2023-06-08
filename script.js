66a5ea9ed4a4d1650828b87ea2247a68a0ce3691
Earl Ryan Parker <earlryanparker@gmail.com>
2023-06-08 00:49:27 -0400

    Added contact-me form functionality in script.js, styles.css to recieve emails

--- script.js
+++ script.js
@@ -5,7 +5,37 @@
 const modal = document.querySelector(".modal");
 const contactMeButton = document.querySelector(".contact_me_button");
 const overlay = document.querySelector(".overlay");
+const closeModalButtton = document.querySelector(".close-modal");
+const submitButton = document.querySelector(".send-button");
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
 
 contactMeButton.addEventListener("click", function () {
-  modal.classList.remove("hidden");
+  openModal();
+});
+
+closeModalButtton.addEventListener("click", function () {
+  closeModal();
+});
+
+overlay.addEventListener("click", function () {
+  closeModal();
+});
+
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
 });
